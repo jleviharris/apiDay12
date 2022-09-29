9354cdce5286a09cd96e6dde5d749b66247c6af1
jleviharris <96702475+jleviharris@users.noreply.github.com>
2022-09-29 17:48:47 -0400

    Day 12 of off days of javaScript Complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [lat, setLat] = useState(null);
+  const [lng, setLng] = useState(null);
+  const [status, setStatus] = useState(null);
+  const [statusClass, setStatusClass] = useState("base");
+
+  // const showPosition = (position) => {
+  //   setLocation({
+  //     Latitude: position.coords.latitude,
+  //     Longitude: position.coords.longitude,
+  //   });
+  // };
+  // const getLocation = () => {
+  //   setLocation(navigator.geolocation.getCurrentPosition());
+  // };
+  const getLocation = () => {
+    if (!navigator.geolocation) {
+      setStatus("Geolocation is not supported by your browser");
+    } else {
+      setStatusClass("active");
+      setStatus("Locating...");
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setStatus(null);
+          setStatusClass("base");
+          setLat(position.coords.latitude);
+          setLng(position.coords.longitude);
+        },
+        () => {
+          setStatus("Unable to retrieve your location");
+        }
+      );
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="box">
+        <button className="btn" onClick={getLocation}>
+          Get Location{" "}
+        </button>
+        <h1>Coordinates</h1>
+        <p className={statusClass}>{status}</p>
+        {lat && <p>Latitude: {lat}</p>}
+        {lng && <p>Longitude: {lng}</p>}
+      </div>
     </div>
   );
 }
